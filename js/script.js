a01478052f99981f2700b98576ce93c2c6d13154
dalastra <ncamposdalastra@gmail.com>
2026-05-12 21:06:23 -0300

    quase pronto

--- js/script.js
+++ js/script.js
@@ -35,3 +35,16 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+const modal = document.getElementById("modal");
+
+const abrirModal = document.getElementById("abrirModal");
+
+const fecharModal = document.getElementById("fecharModal");
+
+abrirModal.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+fecharModal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
